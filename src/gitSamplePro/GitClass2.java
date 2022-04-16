6d3e9350c76b41f4f728c23173cbe82f93794467
Ssatish gawali <Ssatish gawali@DESKTOP-JEATUR9>
2022-04-16 14:36:07 +0530

    changes

--- src/gitSamplePro/GitClass2.java
+++ src/gitSamplePro/GitClass2.java
@@ -1,10 +1,11 @@
-package gitSamplePro1;
+package gitSamplePro;
 
 public class GitClass2 
 {
 	public static void main(String[] args) 
 	{
 		System.out.println("class2 github");
+		System.out.println("changes in github");
 	}
 
 }
