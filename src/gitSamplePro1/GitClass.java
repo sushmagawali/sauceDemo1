757923b1ca2a402c543829ca31a9c259f8360d0c
Ssatish gawali <Ssatish gawali@DESKTOP-JEATUR9>
2022-04-16 14:12:49 +0530

    commiting the first class

---
+++ src/gitSamplePro1/GitClass.java
@@ -0,0 +1,11 @@
+package gitSamplePro1;
+
+public class GitClass 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("git sample project-GitClass");
+		System.out.println("imp github project");
+	}
+
+}
